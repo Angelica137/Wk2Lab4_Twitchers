4bffaabed21322dabefe57a9e1d7b04526393147
Angelica <Angelica137@users.noreply.github.com>
2021-03-23 11:08:05 +0000

    declare array

--- BirdTable.java
+++ BirdTable.java
@@ -10,15 +10,12 @@ import javax.swing.*;
 
 public class BirdTable {
 
+	// main method begins execution of Java application
+	public static void main(String args[]) {
+		// Declare and build an array interactively - see the Temperature Project
+		int[] birds = new int[7];
 
-   // main method begins execution of Java application
-   public static void main( String args[] )
-   {
-      //Declare and build an array interactively - see the Temperature Project
-   
-      
-      
-      //Display Output using a histogram of '*' - see Histogram Project
-   
-   }
+		// Display Output using a histogram of '*' - see Histogram Project
+
+	}
 }
