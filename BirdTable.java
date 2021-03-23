106da80e76479afe453c285dace7497346dd48c0
Angelica <Angelica137@users.noreply.github.com>
2021-03-23 11:18:37 +0000

    declare variables for user input

--- BirdTable.java
+++ BirdTable.java
@@ -13,7 +13,13 @@ public class BirdTable {
 	// main method begins execution of Java application
 	public static void main(String args[]) {
 		// Declare and build an array interactively - see the Temperature Project
+
+		// Declare array
 		int[] birds = new int[7];
+
+		// Declare string element and double element to collect user input
+		String strElement;
+		double doubElement;
 
 		// Display Output using a histogram of '*' - see Histogram Project
 
